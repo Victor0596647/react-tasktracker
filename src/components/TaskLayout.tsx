d1c0953c9956b1d3564171be09a2657a001d2fc3
Victor0596647 <mrvictor059@gmail.com>
2023-12-19 14:31:13 +0200

    implemented form pop up

--- src/components/TaskLayout.tsx
+++ src/components/TaskLayout.tsx
@@ -1,17 +1,54 @@
-import React, { useRef } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import {BsPlus} from 'react-icons/bs'
-import { handleActive } from "./taskFunc";
+import { Task } from "./Task";
+import { getRanHex } from "./hexGen";
 
 import '../styles/task.css';
+import { AddTask } from "./AddTask";
+
+export type TaskObj = {
+    id: string,
+    title: string,
+    desc: string,
+    date: Date
+}
 
 export function TaskLayout():React.ReactElement {
+    const [taskList, setTaskList] = useState<TaskObj[]>([]);
+    const [add, setAdd] = useState<boolean>(false);
     const plusRef = useRef<HTMLSpanElement>(null);
+
+    useEffect(()=> {
+    }, [JSON.stringify(taskList)])
+
+    const handleActive = useCallback(() => {
+        if(plusRef.current) 
+            plusRef.current.classList.toggle('active');
+            setAdd(!add);
+        return
+    }, [add])
+
+    const handleDelete = useCallback((id: string) => {
+        setTaskList(taskList.filter((t) => {return id !== t.id}));
+    }, [taskList])
+
+    const handleAdd = useCallback((title:string, desc:string, date:Date) => {
+        taskList.push({id:getRanHex(8), title:title, desc:desc, date:date});
+        setTaskList(taskList);
+        handleActive();
+        setAdd(false);
+    }, [taskList])
+
     return (
         <div className="taskLayout">
             <div className="header">
                 <h1>Tasks</h1>
-                <span className="plus" ref={plusRef}><BsPlus onClick={() => {handleActive(plusRef)}}/></span>
+                <span className="plus" ref={plusRef}><BsPlus onClick={() => {handleActive()}}/></span>
             </div>
+            {
+                add ? (<AddTask handleAdd={handleAdd} />) : null
+            }
+            <Task taskList={taskList} handleDelete={handleDelete}/>
         </div>
     )
 }
