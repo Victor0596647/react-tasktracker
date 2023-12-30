a17b09fa4c32c18d7ce4e41cd3484a84c7501c76
Victor0596647 <mrvictor059@gmail.com>
2023-12-30 21:51:12 +0200

    implemented `formTaskReducer`
    implemented animation for the form for the opacity

--- src/components/TaskLayout.tsx
+++ src/components/TaskLayout.tsx
@@ -1,35 +1,84 @@
-import React, { useCallback, useEffect, useReducer, useRef, useState } from "react";
+import React, { useCallback, useEffect, useReducer, useRef } from "react";
 import {BsPlus} from 'react-icons/bs'
 import { Task } from "./Task";
 import '../styles/task.css';
 import { AddTask } from "./AddTask";
-import { TaskObj, taskReducer } from "./taskReducer";
+import { TaskObj, formTaskReducer, taskReducer } from "./taskReducer";
 
 export function TaskLayout():React.ReactElement {
     const [taskState, taskDispatch] = useReducer(taskReducer, []);
-    const [add, setAdd] = useState<boolean>(false);
+    const [formState, formDispatch] = useReducer(formTaskReducer, {visible:false, finished: true});
     const plusRef = useRef<HTMLSpanElement>(null);
 
     useEffect(()=> {
         console.log(taskState);
     }, [taskState])
 
+    useEffect(()=> {
+        const formE = document.querySelector('.addTask') as HTMLDivElement;
+        switch(formState.anim_type){
+            case "open":
+                formDispatch({ type:"VISIBILITY", value:true });
+                formE.animate(
+                    [
+                        {
+                            opacity: 0,
+                        },
+                        {
+                            opacity: 100
+                        }
+                    ] as React.CSSProperties[] as Keyframe[],
+                    {
+                        fill:'forwards',
+                        duration:150,
+                        easing:'ease-in'
+                    }
+                ).finished.finally(() => {
+                    formDispatch({ type: "NONE"});
+                })
+                return
+            case "close":
+                formE.animate(
+                    [
+                        {
+                            opacity: 100,
+                        },
+                        {
+                            opacity: 0,
+                        }
+                    ] as React.CSSProperties[] as Keyframe[],
+                    {
+                        fill:'forwards',
+                        duration:150,
+                        easing:'ease-out'
+                    }
+                ).finished.finally(() => {
+                    formDispatch({ type:"VISIBILITY", value:false });
+                    formDispatch({ type: "NONE"});
+                    (document.querySelector(".addTask form") as HTMLFormElement).reset();
+                })
+                return
+            case undefined:
+                return
+        }
+    }, [formState.anim_type])
+
     const handleActive = useCallback(() => {
-        if(plusRef.current)
+        if(plusRef.current && formState.finished){
             plusRef.current.classList.toggle('active');
-            setAdd(!add);
-        return
-    }, [add])
+            formDispatch({ type:(formState.visible ? "CLOSE" : "OPEN") });
+        }
+        return;
+    }, [formState])
 
     const handleDelete = useCallback((task: TaskObj) => {
         taskDispatch({type:'REMOVE', value:task})
     }, [taskState])
 
     const handleAdd = useCallback((task: TaskObj) => {
-        taskDispatch({type:'ADD', value:task})
+        taskDispatch({type:'ADD', value:task});
         handleActive();
-        setAdd(false);
-    }, [taskState])
+    }, [taskState, formState])
 
     return (
         <div className="taskLayout">
@@ -37,9 +86,7 @@ export function TaskLayout():React.ReactElement {
                 <h1>Tasks</h1>
                 <span className="plus" ref={plusRef}><BsPlus onClick={() => {handleActive()}}/></span>
             </div>
-            {
-                add ? (<AddTask handleAdd={handleAdd} />) : null
-            }
+            <AddTask handleAdd={handleAdd} visible={formState.visible} />
             <Task taskList={taskState} handleDelete={handleDelete}/>
         </div>
     )
