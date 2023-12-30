53417fbdea6464a0783d8ced87caa8319961c8cf
Victor0596647 <mrvictor059@gmail.com>
2023-12-30 21:27:59 +0200

    added reducer hook for `addTask` form

--- src/components/taskReducer.ts
+++ src/components/taskReducer.ts
@@ -15,3 +15,30 @@ export const taskReducer = (state:TaskObj[], action: {type:"ADD" | "REMOVE", val
             return state;
     }
 }
+
+type formStateProps = { visible: boolean, finished: boolean, anim_type?: 'open' | 'close' }
+type formActionProps = { type:"OPEN"|"CLOSE"|"NONE" } | { type:"VISIBILITY", value:boolean }
+
+export const formTaskReducer = (state: formStateProps, action: formActionProps): formStateProps => {
+    switch(action.type){
+        case "OPEN":
+            return {
+                ...state,
+                finished:false,
+                anim_type:'open'
+            }
+        case "CLOSE":
+            return {
+                ...state,
+                finished:false,
+                anim_type:'close'
+            }
+        case "VISIBILITY":
+            return {
+                ...state,
+                visible: action.value
+            }
+        case "NONE":
+            return {...state, finished: true, anim_type:undefined}
+    }
+}
